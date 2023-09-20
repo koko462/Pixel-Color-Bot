43f5139c81e7aaa06970b98bee26aa9c6169bfa1
koko462 <115919610+koko462@users.noreply.github.com>
2023-09-20 21:12:56 +0300

    Add files via upload

---
+++ PixelColorBot/PixelColorBot/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Forms;
+using MessageBox = System.Windows.MessageBox;
+using System.Runtime.InteropServices;
+
+
+namespace PixelColorBot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const UInt32 MOUSEEVENTF_LEFTDOWN = 0 * 0002;
+        private const UInt32 MOUSEEVENTF_LEFTUP = 0 * 0004;
+
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, uint dwExtraInf);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetCursorPos(int x, int y);
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Click()
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+        }
+
+        private void OnButtonSearchPixelClick(object sender, RoutedEventArgs e)
+        {
+            string inputHexColorCode = TextBoxHexColor.Text;           
+            SearchPixel(inputHexColorCode);
+        }
+
+        private void DoubleClickAtPosition(int posX, int posY)
+        {
+            SetCursorPos(posX, posY);
+            Click();
+            System.Threading.Thread.Sleep(250);
+            Click();
+        }
+
+        private bool SearchPixel(string hexcode)
+        {
+            Bitmap bitmap = new Bitmap(SystemInformation.VirtualScreen.Width, SystemInformation.VirtualScreen.Height);
+            Graphics graphics = Graphics.FromImage(bitmap as Image);
+            graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+
+            Color desiredPixelColor = ColorTranslator.FromHtml(hexcode);
+            for (int x = 0; x < SystemInformation.VirtualScreen.Width; x++)
+            {
+                for (int y = 0; y < SystemInformation.VirtualScreen.Height; y++)
+                {
+                    Color currentPixelColor = bitmap.GetPixel(x, y);
+
+                    if (desiredPixelColor == currentPixelColor)
+                    {
+                        MessageBox.Show(String.Format("Found Pixel at {0},{1} - Now set mouse cursor", x, y));
+                        DoubleClickAtPosition(x, y);
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
